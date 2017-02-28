c77ea87f6222586ff996fba20db521d963e7850c
elpinal <elpinal@users.noreply.github.com>
2017-02-28 15:48:57 +0900

    Fix the bug which causes the incorrect position

--- editor/basiceditor.go
+++ editor/basiceditor.go
@@ -37,8 +37,8 @@ func (e *basicEditor) insert(s []rune, at int) {
 	case len(e.buf):
 		e.buf = append(e.buf, s...)
 	default:
-		s = append(e.buf[:at:at], s...)
-		e.buf = append(s, e.buf[at:]...)
+		x := append(e.buf[:at:at], s...)
+		e.buf = append(x, e.buf[at:]...)
 	}
 	if at <= e.pos {
 		e.pos += len(s)
