1090106884f3cd29a4cc0387d23d135d96e7d667
elpinal <elpinal@users.noreply.github.com>
2017-03-01 07:56:05 +0900

    Add a few normal commands

--- coco4/editor/normal.go
+++ coco4/editor/normal.go
@@ -39,9 +39,28 @@ type normalCommand struct {
 }
 
 var normalCommands = []normalCommand{
+	{'$', (*normal).endline, 0},
+	{'0', (*normal).beginline, 0},
+	{'b', (*normal).wordBack, 0},
 	{'h', (*normal).left, 0},
-	{'l', (*normal).right, 0},
 	{'i', (*normal).edit, 0},
+	{'l', (*normal).right, 0},
+	{'w', (*normal).word, 0},
+}
+
+func (e *normal) endline() mode {
+	e.move(len(e.buf) - 1)
+	return modeNormal
+}
+
+func (e *normal) beginline() mode {
+	e.move(0)
+	return modeNormal
+}
+
+func (e *normal) wordBack() mode {
+	e.wordBackward()
+	return modeNormal
 }
 
 func (e *normal) left() mode {
@@ -49,11 +68,16 @@ func (e *normal) left() mode {
 	return modeNormal
 }
 
+func (e *normal) edit() mode {
+	return modeInsert
+}
+
 func (e *normal) right() mode {
 	e.move(e.pos + 1)
 	return modeNormal
 }
 
-func (e *normal) edit() mode {
-	return modeInsert
+func (e *normal) word() mode {
+	e.wordForward()
+	return modeNormal
 }
