47ef16a3b10449d391af0d903e407bc51a318c8b
elpinal <elpinal@users.noreply.github.com>
2017-03-01 07:41:12 +0900

    Add mode

---
+++ coco4/editor/mode.go
@@ -0,0 +1,26 @@
+package editor
+
+type mode int
+
+const (
+	modeNormal mode = iota + 1
+	modeVisual
+	modeSelect
+	modeInsert
+	modeCommandline
+	modeEx
+
+	modeOperatorPending
+	modeReplace
+	modeVirtualReplace
+	modeInsertNormal
+	modeInsertVisual
+	modeInsertSelect
+)
+
+type moder interface {
+	Mode() mode
+	Run() (end bool, next mode, err error)
+	Runes() []rune
+	Position() int
+}
