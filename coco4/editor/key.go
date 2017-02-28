5741bf2eb88678b6c7a356ad96cb5ae476f8c533
elpinal <elpinal@users.noreply.github.com>
2017-03-01 07:39:41 +0900

    Add keys

---
+++ coco4/editor/key.go
@@ -0,0 +1,34 @@
+package editor
+
+const (
+	CharCtrlAt = iota
+	CharCtrlA
+	CharCtrlB
+	CharCtrlC
+	CharCtrlD
+	CharCtrlE
+	CharCtrlF
+	CharCtrlG
+	CharCtrlH
+	CharCtrlI
+	CharCtrlJ
+	CharCtrlK
+	CharCtrlL
+	CharCtrlM
+	CharCtrlN
+	CharCtrlO
+	CharCtrlP
+	CharCtrlQ
+	CharCtrlR
+	CharCtrlS
+	CharCtrlT
+	CharCtrlU
+	CharCtrlV
+	CharCtrlW
+	CharCtrlX
+	CharCtrlY
+	CharCtrlZ
+	CharEscape
+
+	CharBackspace = 127
+)
