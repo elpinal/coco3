d86b4e2a5661529c1e17cf301bf68ba37bbc0791
elpinal <elpinal@users.noreply.github.com>
2017-03-12 20:50:18 +0900

    Update test for cli

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -7,15 +7,15 @@ import (
 
 func TestFlagC(t *testing.T) {
 	var in, out, err bytes.Buffer
-	c := cli{
-		in:  &in,
-		out: &out,
-		err: &err,
+	c := CLI{
+		In:  &in,
+		Out: &out,
+		Err: &err,
 	}
 	args := []string{"-c", "echo aaa"}
-	code := c.run(args)
+	code := c.Run(args)
 	if code != 0 {
-		t.Errorf("run: got %v, want %v", code, 0)
+		t.Errorf("Run: got %v, want %v", code, 0)
 	}
 	if got, want := out.String(), "aaa\n"; got != want {
 		t.Errorf("output: got %v, want %v", got, want)
@@ -24,15 +24,15 @@ func TestFlagC(t *testing.T) {
 
 func TestArgs(t *testing.T) {
 	var in, out, err bytes.Buffer
-	c := cli{
-		in:  &in,
-		out: &out,
-		err: &err,
+	c := CLI{
+		In:  &in,
+		Out: &out,
+		Err: &err,
 	}
 	args := []string{"testdata/basic.coco"}
-	code := c.run(args)
+	code := c.Run(args)
 	if code != 0 {
-		t.Errorf("run: got %v, want %v", code, 0)
+		t.Errorf("Run: got %v, want %v", code, 0)
 	}
 	if got, want := out.String(), "aaa\nbbb\n"; got != want {
 		t.Errorf("output: got %v, want %v", got, want)
