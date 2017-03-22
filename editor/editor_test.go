fee998af75ae8ac31a2e04444698fa180a9e0afe
elpinal <elpinal@users.noreply.github.com>
2017-03-22 09:55:24 +0900

    Add test for editor

---
+++ editor/editor_test.go
@@ -0,0 +1,32 @@
+package editor
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/elpinal/coco3/config"
+)
+
+type testScreen struct {
+}
+
+func (ts *testScreen) Refresh(prompt string, s []rune, pos int) {
+}
+
+func (ts *testScreen) SetLastLine(msg string) {
+}
+
+func TestEditor(t *testing.T) {
+	inBuf := strings.NewReader("aaa" + string(CharCtrlM))
+	var outBuf, errBuf bytes.Buffer
+	e := New(&testScreen{}, &config.Config{}, inBuf, &outBuf, &errBuf)
+	s, err := e.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	if want := "aaa"; string(s) != want {
+		t.Errorf("got %q, want %q", string(s), want)
+	}
+	e.Clear()
+}
