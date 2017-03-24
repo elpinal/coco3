4cc2c78bcab43716500728caa55b02672baea474
elpinal <elpinal@users.noreply.github.com>
2017-03-24 21:09:33 +0900

    Add a test case for eval.Evaluator.execPipe

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -18,3 +18,27 @@ func TestExecCmd(t *testing.T) {
 		t.Errorf("error should be blank; got %q", got)
 	}
 }
+
+func TestExecPipe(t *testing.T) {
+	var in, out, err bytes.Buffer
+	e := New(&in, &out, &err)
+	if err := e.execPipe([][]string{
+		[]string{
+			"echo",
+			"aaa",
+		},
+		[]string{
+			"tr",
+			"a",
+			"A",
+		},
+	}); err != nil {
+		t.Errorf("execute pipe: %v", err)
+	}
+	if got, want := out.String(), "AAA\n"; got != want {
+		t.Errorf("output: got %q, want %q", got, want)
+	}
+	if got := err.String(); got != "" {
+		t.Errorf("error should be blank; got %q", got)
+	}
+}
