7aafbc0126c72c03dc41d1133cf6fd198ba414ce
elpinal <elpinal@users.noreply.github.com>
2017-03-28 16:17:26 +0900

    Fix RuneAddReader finally

--- editor/reader.go
+++ editor/reader.go
@@ -2,47 +2,30 @@ package editor
 
 import (
 	"io"
-	"sync"
 	"unicode/utf8"
 )
 
 type RuneAddReader struct {
 	ird io.RuneReader
-	ch  chan rune
-	wg  *sync.WaitGroup
+	s   []rune
+	n   int
 }
 
 func NewReader(rd io.RuneReader) *RuneAddReader {
-	return &RuneAddReader{ird: rd, ch: make(chan rune), wg: &sync.WaitGroup{}}
+	return &RuneAddReader{ird: rd}
 }
 
 func (rd *RuneAddReader) ReadRune() (r rune, size int, err error) {
-	done := make(chan struct{}, 1)
-	go func() {
-		rd.wg.Wait()
-		select {
-		case <-done:
-			return
-		default:
-		}
-		r, size, err = rd.ird.ReadRune()
-		done <- struct{}{}
-	}()
-	select {
-	case r = <-rd.ch:
-		done <- struct{}{}
-		size = utf8.RuneLen(r)
-	case <-done:
+	if len(rd.s) <= rd.n {
+		return rd.ird.ReadRune()
 	}
+	r = rd.s[rd.n]
+	size = utf8.RuneLen(r)
+	rd.n++
 	return r, size, err
 }
 
 func (rd *RuneAddReader) Add(s []rune) {
-	rd.wg.Add(len(s))
-	go func() {
-		for _, r := range s {
-			rd.ch <- r
-			rd.wg.Done()
-		}
-	}()
+	rd.s = append(rd.s[rd.n:], s...)
+	rd.n = 0
 }
--- editor/reader_test.go
+++ editor/reader_test.go
@@ -27,3 +27,27 @@ func TestRuneAddReader(t *testing.T) {
 		}
 	}
 }
+
+func TestRuneAddReaderMultiple(t *testing.T) {
+	rd := NewReader(bufio.NewReaderSize(strings.NewReader(strings.Repeat("a", 128)), 64))
+	r, _, err := rd.ReadRune()
+	if err != nil {
+		t.Errorf("ReadRune: %v", err)
+	}
+	if r != 'a' {
+		t.Errorf("ReadRune: got %v, want %v", r, 'a')
+	}
+	for range [...]int{127: 0} {
+		rd.Add([]rune("dl"))
+	}
+	want := []rune(strings.Repeat("dl", 128) + strings.Repeat("a", 127))
+	for i := 0; i < len(want); i++ {
+		r, _, err = rd.ReadRune()
+		if err != nil {
+			t.Errorf("ReadRune: %v", err)
+		}
+		if r != want[i] {
+			t.Errorf("ReadRune: got %v, want %v", string(r), string(want[i]))
+		}
+	}
+}
