3dc75962af468a7ac2db7c6df39d1239c22167cd
elpinal <elpinal@users.noreply.github.com>
2017-07-03 08:01:18 +0900

    Strengthen the function of aliases a little
    Support aliases like this: `let LANG '' in ...`.

--- eval/alias.go
+++ eval/alias.go
@@ -14,6 +14,11 @@ func DefAlias(name, arg string) {
 	a := strings.Split(arg, " ")
 	cmd := a[0]
 	args := a[1:]
+	for i := range args {
+		if args[i] == "''" {
+			args[i] = ""
+		}
+	}
 	if x, ok := aliases[cmd]; ok {
 		cmd = x.cmd
 		args = append(x.args, args...)
