20a799b2507821c29bad921e9d489983484ad7f3
elpinal <elpinal@users.noreply.github.com>
2017-07-11 18:21:17 +0900

    Revive undo-tree

---
+++ editor/undotree.go
@@ -0,0 +1,68 @@
+package editor
+
+import "time"
+
+type node struct {
+	data   []rune
+	parent *node
+	child  *node
+	when   time.Time
+}
+
+type undoTree struct {
+	current *node
+	nodes   []*node // in chronological order
+}
+
+func newUndoTree() undoTree {
+	return undoTree{current: &node{}}
+}
+
+func (u *undoTree) undo() []rune {
+	if u.current.parent == nil {
+		return nil
+	}
+	u.current = u.current.parent
+	return u.current.data
+}
+
+func (u *undoTree) redo() []rune {
+	if u.current == nil {
+		return nil
+	}
+	u.current = u.current.child
+	return u.current.data
+}
+
+func (u *undoTree) earlier() []rune {
+	for i, n := range u.nodes {
+		if n == u.current {
+			if i == 0 {
+				return nil
+			}
+			u.current = u.nodes[i-1]
+			return u.current.data
+		}
+	}
+	panic("unexpected loss of undo node")
+}
+
+func (u *undoTree) later() []rune {
+	for i, n := range u.nodes {
+		if n == u.current {
+			if i == len(u.nodes)-1 {
+				return nil
+			}
+			u.current = u.nodes[i+1]
+			return u.current.data
+		}
+	}
+	panic("unexpected loss of undo node")
+}
+
+func (u *undoTree) add(s []rune) {
+	n := &node{data: s, parent: u.current, when: time.Now()}
+	u.current.child = n
+	u.current = n
+	u.nodes = append(u.nodes, n)
+}
---
+++ editor/undotree_test.go
@@ -0,0 +1,42 @@
+package editor
+
+import "testing"
+
+func TestUndoTree(t *testing.T) {
+	s1 := "The first history"
+	s2 := "The second history"
+	s3 := "The third history"
+	u := newUndoTree()
+
+	u.add([]rune(s1))
+	if got, want := string(u.undo()), ""; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	u.add([]rune(s2))
+	u.add([]rune(s3))
+	if got, want := string(u.undo()), s2; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	u.add([]rune(s2))
+	if got, want := string(u.undo()), s2; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	if got, want := string(u.redo()), s2; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	if got, want := string(u.later()), ""; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	if got, want := string(u.earlier()), s3; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	if got, want := string(u.earlier()), s2; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	if got, want := string(u.earlier()), s1; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	if got, want := string(u.earlier()), ""; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
