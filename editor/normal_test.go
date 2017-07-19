f4131509b2b34a13a464f973e54e143b6de980d2
elpinal <elpinal@users.noreply.github.com>
2017-07-19 19:05:04 +0900

    Add benchmark for Normal mode

---
+++ editor/normal_test.go
@@ -0,0 +1,23 @@
+package editor
+
+import (
+	"context"
+	"strings"
+	"testing"
+)
+
+func BenchmarkNormal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		command := "hjkl"
+		ed := normal{
+			streamSet: streamSet{in: NewReaderContext(context.TODO(), strings.NewReader(command))},
+			editor:    newEditor(),
+		}
+		for range command {
+			_, _, err := ed.Run()
+			if err != nil {
+				b.Errorf("normal: %v", err)
+			}
+		}
+	}
+}
