1bc31ba8bdc52281d7945a34f579ec7fd53640f5
elpinal <elpinal@users.noreply.github.com>
2017-07-19 19:16:14 +0900

    Use a longer command to measure performance properly

--- editor/normal_test.go
+++ editor/normal_test.go
@@ -8,7 +8,7 @@ import (
 
 func BenchmarkNormal(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		command := "hjkl"
+		command := strings.Repeat("hjkl", 100)
 		ed := normal{
 			streamSet: streamSet{in: NewReaderContext(context.TODO(), strings.NewReader(command))},
 			editor:    newEditor(),
