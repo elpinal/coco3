25670e11e1544546b45c0add7db1931bfaeaa6f8
elpinal <elpinal@users.noreply.github.com>
2017-07-20 20:15:40 +0900

    Add test for command-line

---
+++ editor/commandline_test.go
@@ -0,0 +1,22 @@
+package editor
+
+import (
+	"bytes"
+	"context"
+	"testing"
+)
+
+func TestCommandline(t *testing.T) {
+	command := append([]byte("quit"), CharCtrlM)
+	ed := commandline{
+		streamSet: streamSet{in: NewReaderContext(context.TODO(), bytes.NewReader(command))},
+		editor:    newEditor(),
+	}
+	end, _, err := ed.Run()
+	if err != nil {
+		t.Errorf("commandline: %v", err)
+	}
+	if end != exit {
+		t.Errorf("commandline (%q): want %v, but got %v", command, exit, end)
+	}
+}
