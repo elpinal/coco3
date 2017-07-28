b50dea9c00d41c9c303da5f25fb1544a07cbae8c
elpinal <elpinal@users.noreply.github.com>
2017-07-28 21:38:16 +0900

    Add help.go for editor

---
+++ editor/help.go
@@ -0,0 +1,21 @@
+package editor
+
+import (
+	"fmt"
+	"io"
+)
+
+func help(w io.Writer) error {
+	for k, v := range helpMap {
+		_, err := w.Write([]byte(fmt.Sprintf("%s   %s\n", k, v)))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var helpMap = map[string]string{
+	"h": "left",
+	"l": "right",
+}
