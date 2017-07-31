4220d7ed561659c9b9fe8fe76d6f99d935db5a7e
elpinal <elpinal@users.noreply.github.com>
2017-07-31 23:41:33 +0900

    Use slice instead of map

--- editor/help.go
+++ editor/help.go
@@ -6,8 +6,8 @@ import (
 )
 
 func Help(w io.Writer) error {
-	for k, v := range quickref {
-		_, err := w.Write([]byte(fmt.Sprintf("%-12s%s\n", k, v)))
+	for _, x := range quickref {
+		_, err := w.Write([]byte(fmt.Sprintf("%-12s%s\n", x.k, x.v)))
 		if err != nil {
 			return err
 		}
@@ -16,57 +16,57 @@ func Help(w io.Writer) error {
 }
 
 // Reference: Vim's quickref.txt.
-var quickref = map[string]string{
-	"h": "left",
-	"l": "right",
-	"0": "to first character in the line",
-	"^": "to first non-blank character in the line",
-	"$": "to the last character in the line",
-	"|": "to column N",
-	"f": "to the Nth occurrence of {char} to the right",
-	"F": "to the Nth occurrence of {char} to the left",
+var quickref = []struct{ k, v string }{
+	{"h", "left"},
+	{"l", "right"},
+	{"0", "to first character in the line"},
+	{"^", "to first non-blank character in the line"},
+	{"$", "to the last character in the line"},
+	{"|", "to column N"},
+	{"f", "to the Nth occurrence of {char} to the right"},
+	{"F", "to the Nth occurrence of {char} to the left"},
 
-	"k": "go back history",
-	"j": "go forward history",
+	{"k", "go back history"},
+	{"j", "go forward history"},
 
-	"-": "decrement the number at or after the cursor",
-	"+": "increment the number at or after the cursor",
+	{"-", "decrement the number at or after the cursor"},
+	{"+", "increment the number at or after the cursor"},
 
-	"w": "N words forward",
-	"W": "N blank-separated WORDs forward",
-	"e": "forward to the end of the Nth word",
-	"E": "forward to the end of the Nth blank-separated WORD",
-	"b": "N words backward",
-	"B": "N blank-separated WORDs backward",
+	{"w", "N words forward"},
+	{"W", "N blank-separated WORDs forward"},
+	{"e", "forward to the end of the Nth word"},
+	{"E", "forward to the end of the Nth blank-separated WORD"},
+	{"b", "N words backward"},
+	{"B", "N blank-separated WORDs backward"},
 
-	"/": "search forward",
-	"?": "search backward",
+	{"/", "search forward"},
+	{"?", "search backward"},
 
-	"n": "repeat last search",
-	"N": "repeat last search, in opposite direction",
+	{"n", "repeat last search"},
+	{"N", "repeat last search, in opposite direction"},
 
-	"a":  "append text after the cursor",
-	"A":  "append text at the end of the line",
-	"i":  "insert text before the cursor",
-	"I":  "insert text before the first non-blank in the line",
-	"gI": "insert text in column 1",
+	{"a", "append text after the cursor"},
+	{"A", "append text at the end of the line"},
+	{"i", "insert text before the cursor"},
+	{"I", "insert text before the first non-blank in the line"},
+	{"gI", "insert text in column 1"},
 
 	// insert mode...
 
-	"i_<Esc>":  "end Insert mode, back to Normal mode",
-	"i_CTRL-C": "like <Esc>",
+	{"i_<Esc>", "end Insert mode, back to Normal mode"},
+	{"i_CTRL-C", "like <Esc>"},
 
-	"i_CTRL-R": "insert the contents of a register",
-	"i_CTRL-Y": "complete the word before the cursor in various ways",
-	"i_<BS":    "delete the character before the cursor",
-	"i_CTRL-W": "delete word before the cursor",
-	"i_CTRL-U": "delete all entered characters in the current line",
+	{"i_CTRL-R", "insert the contents of a register"},
+	{"i_CTRL-Y", "complete the word before the cursor in various ways"},
+	{"i_<BS", "delete the character before the cursor"},
+	{"i_CTRL-W", "delete word before the cursor"},
+	{"i_CTRL-U", "delete all entered characters in the current line"},
 
-	"x":     "delete N characters under and after the cursor",
-	"<Del>": "delete N characters under and after the cursor",
-	"X":     "delete N characters before the cursor",
-	"d":     "delete the text that is moved over with {motion}",
-	"v_d":   "delete the highlighted text",
-	"dd":    "delete N lines",
-	"D":     "delete to the end of the line (and N-1 more lines)",
+	{"x", "delete N characters under and after the cursor"},
+	{"<Del>", "delete N characters under and after the cursor"},
+	{"X", "delete N characters before the cursor"},
+	{"d", "delete the text that is moved over with {motion}"},
+	{"v_d", "delete the highlighted text"},
+	{"dd", "delete N lines"},
+	{"D", "delete to the end of the line (and N-1 more lines)"},
 }
