5ea9c92c063fb7365eba4cb023e635057ca9d5b2
elpinal <elpinal@users.noreply.github.com>
2017-10-04 22:15:27 +0900

    Add List & its implementations, Cons & Empty

--- extra/ast/ast.go
+++ extra/ast/ast.go
@@ -6,3 +6,22 @@ type Command struct {
 	Name string
 	Args []token.Token
 }
+
+type List interface {
+	Length() int
+}
+
+type Empty struct{}
+
+func (e *Empty) Length() int {
+	return 0
+}
+
+type Cons struct {
+	Head string
+	Tail List
+}
+
+func (c *Cons) Length() int {
+	return 1 + c.Tail.Length()
+}
