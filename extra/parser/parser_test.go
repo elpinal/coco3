943f138d2f639ed495a40166182d024613b54beb
elpinal <elpinal@users.noreply.github.com>
2017-10-04 22:47:11 +0900

    Update test for Parse

--- extra/parser/parser_test.go
+++ extra/parser/parser_test.go
@@ -8,13 +8,29 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	src := "aa 'b'"
-	x, err := Parse([]byte(src))
-	if err != nil {
-		t.Errorf("Parse: %v", err)
+	tests := []struct {
+		src  string
+		want ast.Command
+	}{
+		{
+			src:  "aa 'b'",
+			want: ast.Command{Name: "aa", Args: []ast.Expr{&ast.String{Lit: "b"}}},
+		},
+		{
+			src: "a [] []",
+			want: ast.Command{Name: "a", Args: []ast.Expr{
+				&ast.Empty{},
+				&ast.Empty{},
+			}},
+		},
 	}
-	want := ast.Command{Name: "aa", Args: []ast.Expr{&ast.String{Lit: "b"}}}
-	if !reflect.DeepEqual(*x, want) {
-		t.Errorf("Parse(%s) != %v; got %v", src, want, x)
+	for _, test := range tests {
+		x, err := Parse([]byte(test.src))
+		if err != nil {
+			t.Errorf("Parse(%q): %v", test.src, err)
+		}
+		if !reflect.DeepEqual(*x, test.want) {
+			t.Errorf("Parse(%q) != %v; got %v", test.src, test.want, x)
+		}
 	}
 }
