fdfb633a25003e1813209dcb085af2e4a3f32b67
elpinal <elpinal@users.noreply.github.com>
2017-10-05 20:48:03 +0900

    Implement fmt.Stringer for Type

--- extra/types/types.go
+++ extra/types/types.go
@@ -8,3 +8,17 @@ const (
 	Ident
 	StringList
 )
+
+func (t Type) String() string {
+	switch t {
+	case String:
+		return "String"
+	case Int:
+		return "Int"
+	case Ident:
+		return "Ident"
+	case StringList:
+		return "List String"
+	}
+	panic("unreachable")
+}
