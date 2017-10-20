d2f31240cfed5dad440deff71bf9392275fdb001
elpinal <elpinal@users.noreply.github.com>
2017-10-20 21:51:58 +0900

    Unify two test functions

--- editor/insert_test.go
+++ editor/insert_test.go
@@ -1,45 +1,46 @@
 package editor
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
 
-func TestInsertMode(t *testing.T) {
-	i := insert{}
-	i.init()
-	input := "abc"
-	i.in = NewReader(strings.NewReader(input))
-	for n := range input {
-		c, _, err := i.Run()
-		if err != nil {
-			t.Errorf("Run: %v", err)
-		}
-		if c != cont {
-			t.Errorf("Run: want %v, but got %v", cont, c)
-		}
-		if got, want := string(i.Runes()), input[:n+1]; got != want {
-			t.Errorf("Run: want %v, but got %v", want, got)
+func testInsertMode(input string, tt []string) func(*testing.T) {
+	return func(t *testing.T) {
+		i := insert{}
+		i.init()
+		i.in = NewReader(strings.NewReader(input))
+		for n := range input {
+			c, _, err := i.Run()
+			if err != nil {
+				t.Errorf("Run: %v", err)
+			}
+			if c != cont {
+				t.Errorf("Run: want %v, but got %v", cont, c)
+			}
+			if got, want := string(i.Runes()), tt[n]; got != want {
+				t.Errorf("Run/%d: want %q, but got %q", n, want, got)
+			}
 		}
 	}
 }
 
 func TestInputMatches(t *testing.T) {
-	i := insert{}
-	i.init()
-	input := "a 'b"
-	i.in = NewReader(strings.NewReader(input))
-	tt := []string{"a", "a ", "a ''", "a 'b'"}
-	for n := range input {
-		c, _, err := i.Run()
-		if err != nil {
-			t.Errorf("Run: %v", err)
-		}
-		if c != cont {
-			t.Errorf("Run: want %v, but got %v", cont, c)
-		}
-		if got, want := string(i.Runes()), tt[n]; got != want {
-			t.Errorf("Run/%d: want %q, but got %q", n, want, got)
-		}
+	tests := []struct {
+		input  string
+		expect []string
+	}{
+		{
+			input:  "abc",
+			expect: []string{"a", "ab", "abc"},
+		},
+		{
+			input:  "a 'b",
+			expect: []string{"a", "a ", "a ''", "a 'b'"},
+		},
+	}
+	for n, test := range tests {
+		t.Run(fmt.Sprintf("%d", n), testInsertMode(test.input, test.expect))
 	}
 }
