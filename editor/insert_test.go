3156ea20e4eb7f7a7cc160312aac9029184e6c62
elpinal <elpinal@users.noreply.github.com>
2017-10-20 20:06:52 +0900

    Add test for insert mode

---
+++ editor/insert_test.go
@@ -0,0 +1,25 @@
+package editor
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestInsertMode(t *testing.T) {
+	i := insert{}
+	i.init()
+	input := "abc"
+	i.in = NewReader(strings.NewReader(input))
+	for n := range input {
+		c, _, err := i.Run()
+		if err != nil {
+			t.Errorf("Run: %v", err)
+		}
+		if c != cont {
+			t.Errorf("Run: want %v, but got %v", cont, c)
+		}
+		if got, want := string(i.Runes()), input[:n+1]; got != want {
+			t.Errorf("Run: want %v, but got %v", want, got)
+		}
+	}
+}
