21fb4878fff9d58759bebca6b6c5ccb094ee4d29
elpinal <elpinal@users.noreply.github.com>
2017-10-20 21:43:38 +0900

    Add test for input of matching characters

--- editor/insert_test.go
+++ editor/insert_test.go
@@ -23,3 +23,23 @@ func TestInsertMode(t *testing.T) {
 		}
 	}
 }
+
+func TestInputMatches(t *testing.T) {
+	i := insert{}
+	i.init()
+	input := "a 'b"
+	i.in = NewReader(strings.NewReader(input))
+	tt := []string{"a", "a ", "a ''", "a 'b'"}
+	for n := range input {
+		c, _, err := i.Run()
+		if err != nil {
+			t.Errorf("Run: %v", err)
+		}
+		if c != cont {
+			t.Errorf("Run: want %v, but got %v", cont, c)
+		}
+		if got, want := string(i.Runes()), tt[n]; got != want {
+			t.Errorf("Run/%d: want %q, but got %q", n, want, got)
+		}
+	}
+}
