6d3b339d5ee336439ae890f986316b60e6498de2
elpinal <elpinal@users.noreply.github.com>
2017-10-21 10:41:02 +0900

    Update test for Command.Signature

--- extra/typed/command_test.go
+++ extra/typed/command_test.go
@@ -7,10 +7,32 @@ import (
 )
 
 func TestSignature(t *testing.T) {
-	cmd := Command{Params: []types.Type{types.Int, types.String}}
-	got := string(cmd.Signature())
-	want := "Int -> String"
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	tests := []struct {
+		ts   []types.Type
+		want string
+	}{
+		{
+			ts:   []types.Type{},
+			want: "",
+		},
+		{
+			ts:   []types.Type{types.Int},
+			want: "Int",
+		},
+		{
+			ts:   []types.Type{types.Int, types.String},
+			want: "Int -> String",
+		},
+		{
+			ts:   []types.Type{types.Int, types.String, types.StringList},
+			want: "Int -> String -> List String",
+		},
+	}
+	for i, test := range tests {
+		cmd := Command{Params: test.ts}
+		got := string(cmd.Signature())
+		if got != test.want {
+			t.Errorf("Signature/%d: got %q, want %q", i, got, test.want)
+		}
 	}
 }
