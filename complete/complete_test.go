d392ee907f15f52d5609f2d197e08c6442398ed2
elpinal <elpinal@users.noreply.github.com>
2017-12-14 17:28:39 +0900

    Test complete.fromPath

--- complete/complete_test.go
+++ complete/complete_test.go
@@ -29,3 +29,40 @@ func TestFile(t *testing.T) {
 		}
 	}
 }
+
+func testReadDir(dir string) ([]string, error) {
+	m := map[string][]string{
+		"a": []string{"x1", "x2", "y1"},
+		"b": []string{"x3", "x4", "y2"},
+	}
+	return m[dir], nil
+}
+
+func TestFromPath(t *testing.T) {
+	tests := []struct {
+		input string
+		want  []string
+	}{
+		{
+			input: "x",
+			want:  []string{"1", "2", "3", "4"},
+		},
+		{
+			input: "y",
+			want:  []string{"1", "2"},
+		},
+		{
+			input: "z",
+			want:  []string{},
+		},
+	}
+	for _, test := range tests {
+		list, err := fromPath([]rune(test.input), len(test.input), "a:b", testReadDir)
+		if err != nil {
+			t.Errorf("fromPath: %v", err)
+		}
+		if !reflect.DeepEqual(list, test.want) {
+			t.Errorf("fromPath: want %v, got %v", test.want, list)
+		}
+	}
+}
