65e23b5216ec127ba329a4b855009c1ee41c2526
elpinal <elpinal@users.noreply.github.com>
2017-12-16 23:47:19 +0900

    Make Token have Kind, not Type

--- extra/token/token.go
+++ extra/token/token.go
@@ -3,9 +3,17 @@ package token
 import "github.com/elpinal/coco3/extra/types"
 
 type Token struct {
-	Kind types.Type
-	Lit  string
+	Kind
+	Lit string
 
 	Line   uint
 	Column uint
+}
+
+type Kind struct {
+	types.Type
+}
+
+func KindOf(t types.Type) Kind {
+	return Kind{Type: t}
 }
