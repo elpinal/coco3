894dc38805548f126a58db5dff0af4059eb4b94a
elpinal <elpinal@users.noreply.github.com>
2017-12-16 23:54:45 +0900

    Remove Kind decl
    It is not used.

--- extra/token/token.go
+++ extra/token/token.go
@@ -1,19 +1,8 @@
 package token
 
-import "github.com/elpinal/coco3/extra/types"
-
 type Token struct {
-	Kind
 	Lit string
 
 	Line   uint
 	Column uint
-}
-
-type Kind struct {
-	types.Type
-}
-
-func KindOf(t types.Type) Kind {
-	return Kind{Type: t}
 }
