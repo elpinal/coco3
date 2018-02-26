fb9145b34d813d53ca59d42ac1af990e848b7a14
elpinal <elpinal@users.noreply.github.com>
2018-02-26 23:59:09 +0900

    Check

--- extra/eval.go
+++ extra/eval.go
@@ -466,7 +466,9 @@ func removeFile(ctx context.Context, s string) error {
 			buf := bufio.NewWriter(os.Stdout)
 			_, err = io.Copy(buf, f)
 			f.Close()
-			buf.Flush()
+			if err := buf.Flush(); err != nil {
+				return err
+			}
 			if err != nil {
 				return err
 			}
